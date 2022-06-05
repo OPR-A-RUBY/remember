9e87b4a725b129aaca739f34db8219abedbf27c9
OPR-A-RUBin <opr.a.ruby@gmail.com>
2022-06-05 23:15:01 +0400

    Add clean 'sinatra' pattern and bootstrap 5.1.3
    Добавлен пакет файлов для работы с gem 'sinatra'
    с зависимсостями (другими gems)
    + добавлены файлы из 'bootstrap 5.1.3' (локальная версия) со стилями и скриптами.
    Предусвотановлен Навигационный блок, используемый в layout.erb - типа меню.
    
    Выполнен > bundle install
    p.s.: Application is running. No problems.

--- app.rb
+++ app.rb
@@ -1,1 +1,12 @@
-# app.rb
+#encoding: utf-8
+require 'rubygems'
+require 'sinatra'
+#require 'sinatra/reloader'
+
+get '/' do
+	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School !!!</a>"
+end
+
+get '/about' do
+	erb :about
+end	
