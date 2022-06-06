ca450081270e0315045ca576f60675a702f2eaca
OPR-A-RUBin <opr.a.ruby@gmail.com>
2022-06-07 00:24:52 +0400

    Add tables to db (Users, Person, Photos)
    Добавлены таблицы в базу данных.
    Users - это пользователь сайта (или приглашённый гость)
    Person - это персоны, о которых этот сайт.
    Photos - это фотографии этих персон.

--- helpers/initialize.rb
+++ helpers/initialize.rb
@@ -3,9 +3,37 @@ configure do
   db = get_db
   
   db.execute "CREATE TABLE IF NOT EXISTS 
+  `Users` 
+  (
+    `id`      INTEGER PRIMARY KEY AUTOINCREMENT,
+    `name`       TEXT,
+    `login`		 TEXT,
+    `pass_main`  TEXT,
+    `pass_guest` TEXT
+  ) "
+
+  db.execute "CREATE TABLE IF NOT EXISTS 
   `Persons` 
   (
-    `id`        INTEGER PRIMARY KEY AUTOINCREMENT,
-    `name`      TEXT
+    `id`       INTEGER PRIMARY KEY AUTOINCREMENT,
+    `id_user`	  TEXT,
+    `name`        TEXT,
+    `b_date`	  TEXT,
+    `d_date`      TEXT,
+    `photo_link`  TEXT,
+    `discription` TEXT
   ) "
+
+  db.execute "CREATE TABLE IF NOT EXISTS 
+  `Photos` 
+  (
+    `id`        INTEGER PRIMARY KEY AUTOINCREMENT,
+    `id_person`	   TEXT,
+    `photo_link`   TEXT,
+    `photo_date`   TEXT,
+    `location`	   TEXT,	
+    `person_age`   TEXT,
+    `discription`  TEXT
+  ) "
+  
 end
