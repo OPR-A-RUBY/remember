aeac71e84e6aa0e5e0a18af180e3890dea1a019e
OPR-A-RUBin <opr.a.ruby@gmail.com>
2022-06-07 22:07:27 +0400

    Getting params for User
    Получение параметров полдьзователя из БД

--- app.rb
+++ app.rb
@@ -6,8 +6,15 @@ require 'sqlite3'
 
 require_relative './helpers/for_db.rb' # Методы для работы с БАЗОЙ ДАННЫХ
 require_relative './helpers/initialize.rb' 
+require_relative './helpers/persons.rb'
+require_relative './helpers/users.rb' 
 
 get '/' do
+	debug_user = 'Opr' # Отладочный пользователь
+
+	db = get_db
+	@user = db.execute "SELECT * FROM Users WHERE login=? ", [debug_user]
+
 	erb :index
 end
 
