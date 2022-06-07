57b5b508fb85cbf35fff6e03f8f2d4ce78d1a1b7
OPR-A-RUBin <opr.a.ruby@gmail.com>
2022-06-07 22:45:10 +0400

    FIX links bootstrap
    Добавил в начале каждой ссылки "слэш"
    для получения доступа к файлим bootstrap
    из вложенных ссылок типа: '/person/new'
    
    (без этого слеша файлы были недоступны)

--- helpers/persons.rb
+++ helpers/persons.rb
@@ -1,5 +1,13 @@
 # persons.rb
 
-get 'person/new' do 
-	erb :person_new
-end	
+get '/person/new' do
+  erb :person_new
+end
+
+get '/person/edit' do
+  erb :person_edit
+end
+
+get '/person/delete' do
+  erb :person_delete
+end
