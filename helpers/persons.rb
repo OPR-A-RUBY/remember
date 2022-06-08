c92145450f1f8f7d615d1c1275928e0ca214c41c
OPR-A-RUBin <opr.a.ruby@gmail.com>
2022-06-09 00:19:08 +0400

    Recoding to best usabelity
    Добавим разделения для лучшего прочтения кода.
    Изменим (добвим) пояснения к коду.

--- helpers/persons.rb
+++ helpers/persons.rb
@@ -3,14 +3,16 @@ get '/persons' do
 	erb :persons
 end	
 
+
 get '/person/new' do
   erb :person_new
 end
 
+
 post '/person/new' do
 	@par = params
 	if params['name'].size == 0
-		@error = '"Фамиоия Имя Отчество" - не должно быть пустым.' 
+		@error = '"Фамилия Имя Отчество" - не должно быть пустым.' 
 		return erb :person_new
 	end	
 	db = get_db
@@ -24,14 +26,16 @@ post '/person/new' do
     	  params['discription']
     ]	  
   ]
-	seed_db_Persons db, persons
+	seed_db_Persons db, persons # Запись данных в Базу данных
 	@persons = db.execute "SELECT * FROM Persons WHERE id_user=? ", [$user[0]['id']]
   erb :persons
 end
 
+
 get '/person/edit' do
   erb :person_edit
 end
+
 
 get '/person/delete' do
   erb :person_delete
