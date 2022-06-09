8a38e86f91dedbf4877fa6afb236ee768119d8d9
OPR-A-RUBin <opr.a.ruby@gmail.com>
2022-06-09 23:43:01 +0400

    Add remarks to methods of Persons
    Добавлены коментарии для методов, которые обрабатывают информацию по Персонам.

--- helpers/persons.rb
+++ helpers/persons.rb
@@ -3,12 +3,12 @@ get '/persons' do
 	erb :persons
 end	
 
-
+# Для создании новой персоны - выход на пустую форму для ввода данных
 get '/person/new' do
   erb :person_new
 end
 
-
+# Для обработки введённых данных о новой персоне и сохранении информации в БД
 post '/person/new' do
 	@par = params
 	if params['name'].size == 0
@@ -28,10 +28,11 @@ post '/person/new' do
   ]
 	seed_db_Persons db, persons # Запись данных в Базу данных
 	@persons = db.execute "SELECT * FROM Persons WHERE id_user=? ", [$user[0]['id']]
-  erb :persons
+  erb :persons 
 end
 
 
+# Получение данных об одной из персон и вывод её в форму для редактирования
 get '/person/:id_person' do
   db = get_db
   @person = db.execute "SELECT * FROM Persons WHERE id=? ", [params[:id_person]]
@@ -39,6 +40,7 @@ get '/person/:id_person' do
 end
 
 
+# Удаление информации из БД (перенос информации в другую таблицу БД)
 get '/person/delete' do
   erb :person_delete
 end
