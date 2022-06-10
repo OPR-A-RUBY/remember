e36d123090d07de6e06e83a394b2a1881c33d4cf
OPR-A-RUBin <opr.a.ruby@gmail.com>
2022-06-10 21:24:06 +0400

    FIX - Add gem 'sinatra/reloader' and gem "sinatra-contrib"
    Исправили ошибку при использовании гема reloader
    Который при старте был причиной отказа и поэтому мы его закоментировали его.
    Для его работы нужен ещё один гем "sinatra-contrib"

--- app.rb
+++ app.rb
@@ -1,7 +1,7 @@
 #encoding: utf-8
 require 'rubygems'
 require 'sinatra'
-#require 'sinatra/reloader'
+require 'sinatra/reloader'
 require 'sqlite3'
 
 require_relative './helpers/for_db.rb' # Методы для работы с БАЗОЙ ДАННЫХ
